bd72017b27d39c0b0e7c3d5d165ad8168c5a04fa
Harry <harry.gray@environment-agency.gov.uk>
2025-08-14 17:39:16 +0100

    Added Linear Mixed Effect models, random and fixed for spatial and temp plus ICC for proportion of variance

--- EDA_Mass_Spec_scans.R
+++ EDA_Mass_Spec_scans.R
@@ -49,15 +49,120 @@ setwd("C:/Users/hg000051/Downloads/")
   RSN_Sites_sf <- RSN_Sites %>% st_as_sf(coords = c("GRTS longitude (WGS84)","GRTS latitude (WGS84)"), crs=4326)
   
 # Confirm if site ids match
-  
- RSN_MS <-  Open_Tgt_sf %>% 
-    filter(str_detect(Sample_Site_ID, "RSN"))
+# Extract month and year for temporal variance.
 
+  RSN_MS <-  Open_Tgt_sf %>% 
+    filter(str_detect(Sample_Site_ID, "RSN"))
+  
+ # Observe concentration normality- large right tail
+  ggplot(RSN_MS, aes(x = Concentration)) +
+         geom_histogram(aes(y = ..density..), bins = 50) +
+         geom_density(color = "seagreen2", size = 1)
+  
+  ggplot(RSN_MS, aes(x = logConc)) +
+    geom_histogram(aes(y = ..density..), bins = 50) +
+    geom_density(color = "seagreen2", size = 1)
+  
+
+ # Log transform concentration and capture month to test temporal component
+ 
+  RSN_MS %<>% 
+    mutate(
+      month = as.factor(month(Sample_datetime)),
+      season = case_when(
+               month %in% c(3,4,5) ~ "Spring",
+               month %in% c(6,7,8) ~ "Summer",
+               month %in% c(9,10,11) ~ "Autumn",
+               month %in% c(12,1,2) ~ "Winter"
+      ),
+      logConc = log(Concentration)
+    )
+ 
+ # Test to see if temporal variability is statistically significant and see how it compares to spatial variability 
+
+ 
+  lme1 <- lmer(logConc ~ 1 + (1 | Sample_Site_ID), data = RSN_MS)
+  lme1
+ 
+  # Add seasonality as an effect instead of just spatial. 
+ lme2 <- lme4::lmer(logConc ~ 1 + (1| Sample_Site_ID) + (1 + season), data = RSN_MS)
+ lme2
+ # spring and winter have notably lower concs -0.30 ish whilst summer is 0.03 different to autumn. 
+ 
+ anova(lme1,lme2)
+ # seasonality effect is statistically significant. AIC difference >10 so lme1 better fit.
+ 
+ # Try seasonality as random effect 
+ 
+ # Add seasonality as a random effect instead of just spatial. 
+ lme2r <- lme4::lmer(logConc ~ 1 + (1| Sample_Site_ID) + (1 | season), data = RSN_MS)
+ lme2r
+ 
+ # residuals explains more than spatial or temporal - what we can't account for. 
+ # Spaital explains ~double the amount of variance than temporal. 
+ 
+ lme3 <- lme4::lmer(logConc ~ 1 + (1| Sample_Site_ID) + (1 | month), data = RSN_MS)
+ lme3
+ 
+ # Resid 1.693
+ # concs rise in summer/ autumn months.
+
+ anova(lme2,lme3)
+ # 
+ 
+ # Should probably group by determinand group, like pesticide, PFAS etc.
+ 
+ 
+ 
+ 
+ # ICC after 
+ 
+ vc <- as.data.frame(VarCorr(lme2r))
+ total_var <- sum(vc$vcov)
+  
+    icc_site  <- vc$vcov[vc$grp == "Sample_Site_ID"] / total_var
+ icc_month <- vc$vcov[vc$grp == "month"] / total_var
+  icc_resid <- vc$vcov[vc$grp == "Residual"] / total_var
+  
+    icc_table <- data.frame(
+          Component = c("Spatial (Site)", "Temporal (Month)", "Residual"),
+          Proportion = c(icc_site, icc_month, icc_resid)
+      )
+
+ 
+ #9% variance is spatial
+# 2% is temporal 
+# 90% is within site noise
+ 
+ # spatial variance > temporal variance. More gain from increasing number of sites than frequency at each site. 
+ # Tes
+ 
+ 
+    vc <- as.data.frame(VarCorr(lme2r))
+    total_var <- sum(vc$vcov)
+    
+    # Calculate proportion of variance (ICC)
+    icc_site   <- vc$vcov[vc$grp == "Sample_Site_ID"] / total_var
+    icc_season <- vc$vcov[vc$grp == "season"] / total_var
+    icc_resid  <- vc$vcov[vc$grp == "Residual"] / total_var
+    
+    # Create table for reporting
+    icc_table <- data.frame(
+      Component  = c("Spatial (Site)", "Temporal (Season)", "Residual"),
+      Proportion = c(icc_site, icc_season, icc_resid)
+    )
+    
+    # View ICC table
+    icc_table
+  # More site variance explained than seasonal still, checks out.
+  # temp variation is halved when using seasons to compare temporal variability. Meaning variability between months inside a season gets lost. 
+ # seasonal monitoring would miss within month variability. 
+ 
 # Identify which library compounds we didn't find on the RSN network
  # Do we need to screen for these?
   
   Comps_exc <-  LCGC_Lib %>% 
-          filter(!Compound_Name %in%  RSN_MS$Compound_Name)
+          filter(Compound_Name %in%  RSN_MS$Compound_Name)
   
   Comps_exc %>% distinct()  # list of compounds on the library but not found in the environment.
   
@@ -69,8 +174,11 @@ setwd("C:/Users/hg000051/Downloads/")
     mutate(Detected = TRUE) %>% 
   pivot_wider(names_from = method, values_from = Detected, values_fill = FALSE)
   
-# Identify if there are any national trends
+# 1 
+
+  # Test for temporal vs spatial variability. 
   
+
   
  
   
